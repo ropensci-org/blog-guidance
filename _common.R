cdfe887ebea63a881d9ac2695b0a7b4655b1868e
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-01-23 12:06:37 +0100

    rm duplication

--- _common.R
+++ _common.R
@@ -1,3 +1,17 @@
 knitr::opts_chunk$set(
-  cache = TRUE
+  cache = TRUE,
+  echo = FALSE
 )
+
+library("magrittr")
+
+show_template <- function(filename, lang = "markdown") {
+  suppressWarnings(
+    readLines(
+      file.path("templates", filename)
+    )
+  ) %>%
+    glue::glue_collapse(sep = "\n") -> template
+
+glue::glue("````{lang}\n{template}\n````")
+}
