e41588430611808516f7b4ee520f96303f2c9939
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-24 15:52:17 +0100

    oops

--- inst/scripts/pr.R
+++ inst/scripts/pr.R
@@ -2,13 +2,11 @@ check <- gh::gh("GET /repos/:owner/:repo/commits/:ref/check-runs",
        owner = "ropensci-org",
        repo = "blog-guidance",
        ref = gert::git_log(max = 1)$commit,
-       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"),
-       .token = Sys.getenv("repotoken"))
+       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"))
 
 gh::gh("PATCH /repos/:owner/:repo/check-runs/:check_run_id",
        owner = "ropensci-org",
        repo = "blog-guidance",
        check_run_id = check$check_runs[[1]]$id,
        details_url = "https://masalmon.eu",
-       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"),
-       .token = Sys.getenv("repotoken"))
+       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"))
