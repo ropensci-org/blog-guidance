dcb632f408d7b6d0bad60b06cd9b42649d2529ab
Maëlle Salmon <maelle.salmon@yahoo.se>
2020-02-24 15:08:19 +0100

    try

---
+++ inst/scripts/pr.R
@@ -0,0 +1,12 @@
+check <- gh::gh("GET /repos/:owner/:repo/commits/:ref/check-runs",
+       owner = "ropensci-org",
+       repo = "blog-guidance",
+       ref = gert::git_log(max = 1)$commit,
+       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"))
+
+gh::gh("PATCH /repos/:owner/:repo/check-runs/:check_run_id",
+       owner = "ropensci-org",
+       repo = "blog-guidance",
+       check_run_id = check$check_runs[[1]]$id,
+       details_url = Sys.getenv("DEPLOY_URL"),
+       .send_headers = c(Accept = "application/vnd.github.antiope-preview+json"))
